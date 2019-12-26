3fea93f785aa0866d1bba9650e801e841ed20125
leavemyyard <zhukovpavel2001@gmail.com>
2019-12-26 12:51:27 +0200

    231312

--- classification_model.py
+++ classification_model.py
@@ -24,6 +24,11 @@ train_data = pd.read_csv('data/train_converted.csv').drop('Unnamed: 0', axis=1).
 
 targets = list(pd.read_csv('data/train.csv', index_col=None)['target'])
 
+#train_data = pd.read_csv('data/train_converted.csv').drop('Unnamed: 0', axis=1)
+#test_data = pd.read_csv('data/test_converted.csv').drop('Unnamed: 0', axis=1)
+#targets = np.array([0]*len(train_data) + [1]*len(test_data))
+
+
 for i in range(len(targets)):
     if targets[i] != 0:
         targets[i] = [1, 0]
@@ -31,6 +36,7 @@ for i in range(len(targets)):
         targets[i] = [0, 1]
 
 targets = np.array(targets)
+
 
 def build_model():
     model = Sequential()
@@ -91,48 +97,56 @@ def build_classification_model():
     history = model.fit(
         inputs,
         outputs,
-        epochs=300,
-        callbacks=[early_stop]
+        epochs=30,
+        validation_split=0.2,
+        #callbacks=[early_stop]
     )
 
     #plot_history(history)
 
-    test_predictions = model.predict(train_data.iloc[int(len(train_data)*0.7):])
-    test_labels = targets[int(len(train_data)*0.7):]
+    
+    def test_coef(coef = 0.5):
+        test_predictions = model.predict(train_data.iloc[int(len(train_data)*0.7):])
+        test_labels = targets[int(len(train_data)*0.7):]
 
-    good_predicted_good = 0
-    good_predicted_bad = 0
-    bad_predicted_good = 0
-    bad_predicted_bad = 0
+        good_predicted_good = 0
+        good_predicted_bad = 0
+        bad_predicted_good = 0
+        bad_predicted_bad = 0
 
-    for i, j in zip(test_predictions, test_labels):
-        label = 1 if j[1] == 1 else 0 # 0 means good, 1 means corruption
-        prediction = 0 if i[0] > i[1] else 1
-        if label == prediction == 0:
-            good_predicted_good += 1
-        elif label == prediction == 1:
-            bad_predicted_bad += 1
-        elif label == 1 and prediction == 0:
-            bad_predicted_good += 1
-        elif label == 0 and prediction == 1:
-            good_predicted_bad += 1
+        for i, j in zip(test_predictions, test_labels):
+            label = 1 if j[1] == 1 else 0 # 0 means good, 1 means corruption
+            prediction = 0 if i[0] > coef else 1
+            if label == prediction == 0:
+                good_predicted_good += 1
+            elif label == prediction == 1:
+                bad_predicted_bad += 1
+            elif label == 1 and prediction == 0:
+                bad_predicted_good += 1
+            elif label == 0 and prediction == 1:
+                good_predicted_bad += 1
 
-    total_predictions = good_predicted_good + bad_predicted_bad + good_predicted_bad + bad_predicted_good
-    total_correct = good_predicted_good + bad_predicted_bad
-    total_incorrect = total_predictions - total_correct
-    total_good = good_predicted_good + good_predicted_bad
-    total_bad = bad_predicted_good + bad_predicted_bad
+        total_predictions = good_predicted_good + bad_predicted_bad + good_predicted_bad + bad_predicted_good
+        total_correct = good_predicted_good + bad_predicted_bad
+        total_incorrect = total_predictions - total_correct
+        total_good = good_predicted_good + good_predicted_bad
+        total_bad = bad_predicted_good + bad_predicted_bad
 
-    print('--------------Results-------------')
-    print(f'Correct predictions: {round(100 * total_correct/total_predictions, 5)}% ({total_correct}/{total_predictions})')
-    print(f'Incorrect predictions: {round(100 * total_incorrect/total_predictions, 5)}% ({total_incorrect}/{total_predictions})')
-    print(f'Not corrupted predicted correct: {round(100 * good_predicted_good/total_good, 5)}% ({good_predicted_good}/{total_good})')
-    print(f'Not corrupted predicted incorrect: {round(100 * good_predicted_bad/total_good, 5)}% ({good_predicted_bad}/{total_good})')
-    print(f'Corrupted predicted correct: {round(100 * bad_predicted_bad/total_bad, 5)}% ({bad_predicted_bad}/{total_bad})')
-    print(f'Corrupted predicted incorrect: {round(100 * bad_predicted_good/total_bad, 5)}% ({bad_predicted_good}/{total_bad})')
-    print('---------------------------------')
+        print(f'------------Results {coef}-------------')
+        print(f'Correct predictions: {round(100 * total_correct/total_predictions if total_predictions != 0 else 0, 5)}% ({total_correct}/{total_predictions})')
+        print(f'Incorrect predictions: {round(100 * total_incorrect/total_predictions if total_predictions != 0 else 0, 5)}% ({total_incorrect}/{total_predictions})')
+        print(f'Not corrupted predicted correct: {round(100 * good_predicted_good/total_good if total_predictions != 0 else 0, 5)}% ({good_predicted_good}/{total_good})')
+        print(f'Not corrupted predicted incorrect: {round(100 * good_predicted_bad/total_good if total_predictions != 0 else 0, 5)}% ({good_predicted_bad}/{total_good})')
+        print(f'Corrupted predicted correct: {round(100 * bad_predicted_bad/total_bad if total_predictions != 0 else 0, 5)}% ({bad_predicted_bad}/{total_bad})')
+        print(f'Corrupted predicted incorrect: {round(100 * bad_predicted_good/total_bad if total_predictions != 0 else 0, 5)}% ({bad_predicted_good}/{total_bad})')
+        print('--------------------------------------')
+
+    #for i in np.arange(0, 1, 0.05):
+    #    test_coef(i)
 
     return model
+
+
 
 if __name__ == "__main__":
     build_classification_model()
